f0a6eb9d0b887e23fdee18a07beec4df8af91944
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-03-11 01:40:53 -0300

    creating the frontend with form

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     if (app()->isLocal()) {
         auth()->loginUsingId(1);
-
         return to_route('dashboard');
     } else {
         return view('welcome');
